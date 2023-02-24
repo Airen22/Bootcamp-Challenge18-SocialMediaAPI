76f3481c7c256fe541f4f285e497a69a059ebcae
Erin <erinpeifer@gmail.com>
2023-02-23 20:10:27 -0600

    added utils/helpers; added content to models

--- models/Thought.js
+++ models/Thought.js
@@ -1,21 +1,25 @@
 const { Schema, model } = require('mongoose');
+const format_date = require('../utils/helpers')
+const reactionSchema = require('./Reaction');
 
 const thoughtSchema = new Schema(
     {
         thoughtText: {
-
+            type: String,
+            required: true,
+            maxlength: 280
         },
         createdAt: {
-
+            type: Date,
+            default: Date.now,
+            get:timestamp=>format_date(timestamp)
         },
         username: {
-
+            type: String,
+            required: true
         },
-        reactions: 
-        [
-            {
-                // Array of nested documents created with the reactionSchema
-            }
+        reactions: [
+            reactionSchema
         ]      
     },
     {
--- models/User.js
+++ models/User.js
@@ -3,15 +3,22 @@ const { Schema, model } = require('mongoose');
 const userSchema = new Schema(
     {
     username: {
+        type: String,
+        unique: true, 
+        required: true,
+        trim: true
 
     },
     email: {
-        
+        type: String,
+        unique: true,
+        required: true,
+        match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
     },
     thoughts: [
         {
             type: Schema.Types.ObjectId,
-            ref: 'Thoughts',
+            ref: 'Thought',
         },
     ],
     friends: [
@@ -29,7 +36,7 @@ const userSchema = new Schema(
 );
 
 userSchema
-    .virtual('freindCount')
+    .virtual('friendCount')
     .get(function () {
         return this.friends.length;
     });
